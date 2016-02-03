058a1da5e06dfe57351ba57ea901492ddbc6ba45
mbredel <michael.bredel@cern.ch>
2016-02-03 10:48:23 +0100

    Initial commit.

---
+++ src/test/java/org/github/mbredel/commons/configuration/ConfigurationAssert.java
@@ -0,0 +1,140 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.github.mbredel.commons.configuration;
+
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.ConfigurationRuntimeException;
+import org.junit.Assert;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Iterator;
+
+/**
+ * Assertions on configurations for the unit tests. This class also provides
+ * access to test files.
+ *
+ * @author Emmanuel Bourg
+ * @version $Id: ConfigurationAssert.java 1221893 2011-12-21 21:34:38Z oheger $
+ */
+public class ConfigurationAssert
+{
+    /** Constant for the name of the directory with the test files. */
+    public static final String TEST_DIR_NAME = "target/test-classes";
+
+    /** Constant for the name of the directory with the output files. */
+    public static final String OUT_DIR_NAME = "target";
+
+    /** The directory with the test files. */
+    public static final File TEST_DIR = new File(TEST_DIR_NAME);
+
+    /** The directory with the output files. */
+    public static final File OUT_DIR = new File(OUT_DIR_NAME);
+
+    /**
+     * Checks the content of a configuration.
+     *
+     * @param expected the expected properties
+     * @param actual the configuration to check
+     */
+    @SuppressWarnings("unused")
+    public static void assertEquals(Configuration expected, Configuration actual)
+    {
+        // check that the actual configuration contains all the properties of the expected configuration
+        for (Iterator<String> it = expected.getKeys(); it.hasNext();)
+        {
+            String key = it.next();
+            Assert.assertTrue("The actual configuration doesn't contain the expected key '" + key + "'", actual.containsKey(key));
+            Assert.assertEquals("Value of the '" + key + "' property", expected.getProperty(key), actual.getProperty(key));
+        }
+
+        // check that the actual configuration has no extra properties
+        for (Iterator<String> it = actual.getKeys(); it.hasNext();)
+        {
+            String key = it.next();
+            Assert.assertTrue("The actual configuration contains an extra key '" + key + "'", expected.containsKey(key));
+        }
+    }
+
+    /**
+     * Returns a {@code File} object for the specified test file.
+     *
+     * @param name the name of the test file
+     * @return a {@code File} object pointing to that test file
+     */
+    public static File getTestFile(String name)
+    {
+        return new File(TEST_DIR, name);
+    }
+
+    /**
+     * Returns a {@code File} object for the specified out file.
+     *
+     * @param name the name of the out file
+     * @return a {@code File} object pointing to that out file
+     */
+    public static File getOutFile(String name)
+    {
+        return new File(OUT_DIR, name);
+    }
+
+    /**
+     * Returns a URL pointing to the specified test file. If the URL cannot be
+     * constructed, a runtime exception is thrown.
+     *
+     * @param name the name of the test file
+     * @return the corresponding URL
+     */
+    @SuppressWarnings("unused")
+    public static URL getTestURL(String name)
+    {
+        return urlFromFile(getTestFile(name));
+    }
+
+    /**
+     * Returns a URL pointing to the specified output file. If the URL cannot be
+     * constructed, a runtime exception is thrown.
+     *
+     * @param name the name of the output file
+     * @return the corresponding URL
+     */
+    @SuppressWarnings("unused")
+    public static URL getOutURL(String name)
+    {
+        return urlFromFile(getOutFile(name));
+    }
+
+    /**
+     * Helper method for converting a file to a URL.
+     *
+     * @param file the file
+     * @return the corresponding URL
+     * @throws ConfigurationRuntimeException if the URL cannot be constructed
+     */
+    private static URL urlFromFile(File file)
+    {
+        try
+        {
+            return file.toURI().toURL();
+        }
+        catch (MalformedURLException mex)
+        {
+            throw new ConfigurationRuntimeException(mex);
+        }
+    }
+}
---
+++ src/test/java/org/github/mbredel/commons/configuration/YAMLConfigurationTest.java
@@ -0,0 +1,91 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The names of the contributors may not be used to endorse or promote
+ *  products derived from this software without specific prior written
+ * permission.
+ */
+
+package org.github.mbredel.commons.configuration;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.junit.Before;
+import org.junit.Test;
+import java.io.File;
+import java.io.StringWriter;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Michael Bredel.
+ */
+public class YAMLConfigurationTest {
+    /** Constant for the used encoding.*/
+    static final String ENCODING = "ISO-8859-1";
+
+    /** Constant for the number of test threads. */
+    private static final int THREAD_COUNT = 5;
+
+    /** Constant for the number of loops in tests with multiple threads. */
+    private static final int LOOP_COUNT = 100;
+
+    /** The files that we test with. */
+    private String testYaml = ConfigurationAssert.getTestFile("test.yaml").getAbsolutePath();
+    private File testSaveConf = ConfigurationAssert.getOutFile("testsave.yaml");
+
+    private YAMLConfiguration yamlConfiguration;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        yamlConfiguration = new YAMLConfiguration();
+        yamlConfiguration.setFile(new File(testYaml));
+        yamlConfiguration.load();
+        removeTestFile();
+    }
+
+    @Test
+    public void testGetProperty()
+    {
+        assertEquals("value1", yamlConfiguration.getProperty("key1"));
+        assertEquals("value23", yamlConfiguration.getProperty("key2.key3"));
+    }
+
+    @Test
+    public void testSave() {
+        try {
+            yamlConfiguration.save(new StringWriter());
+        } catch (ConfigurationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Removes the test output file if it exists.
+     */
+    private void removeTestFile()
+    {
+        if (testSaveConf.exists())
+        {
+            assertTrue(testSaveConf.delete());
+        }
+    }
+}
